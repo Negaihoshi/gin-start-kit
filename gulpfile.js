ffacc6d7feeb0e379d52dfd0c81591b9144ddae3
Negaihoshi <e222et@gmail.com>
2015-09-09 16:46:16 +0800

    add multi- templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,13 @@
 var gulp = require('gulp');
-var inject = require('gulp-inject');
 var wiredep = require('wiredep').stream;
-var jade = require('gulp-jade');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('bower', function () {
-  gulp.src('./templates/layout.jade')
+gulp.task('bower', function() {
+  gulp.src('./templates/layout.tmpl')
     .pipe(wiredep({
       optional: 'configuration',
-      goes: 'here'
+      goes: 'here',
     }))
     .pipe(gulp.dest('./templates/'));
 });
 
-
-
-gulp.task('templates', function() {
-
-  gulp.src('./templates/*.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./.tmp/public/template/'));
-});
-
-gulp.task('default', ['bower', 'templates']);
+gulp.task('default', ['bower']);
--- router.go
+++ router.go
@@ -3,20 +3,26 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-gonic/contrib/renders/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
 func routerService() {
-
 	router.Static("/public/css/", "./public/css")
 	router.Static("/public/js/", "./public/js/")
 	router.Static("/public/font/", "./public/fonts/")
 	router.Static("/public/img/", "./public/img/")
 	router.Static("/public/template/", "./tmp/public/template")
 
-	router.LoadHTMLGlob("templates/index.tmpl")
+	templates := multitemplate.New()
+	templates.AddFromFiles("index",
+		"templates/layout.tmpl",
+		"templates/index.tmpl")
+
+	router.HTMLRender = templates
+
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index", gin.H{
 			"title": "Main website",
 		})
 	})
